083c4a93995c6cbd99ddf6abe79aa397088a39ba
alan-stall <alan_kstall@hotmail.com>
2023-05-18 21:16:48 -0300

    feat: add first path for test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify';
 
 const app = fastify();
+
+app.get('/hello', () => {
+    return 'Hello World'
+});
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('🚀 🚀 🚀 HTTP server running on http://localhost:3333')
+})
