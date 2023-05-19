a5c65f98cb2bec95be26d0ce0dab13e324ffc188
alan-stall <alan_kstall@hotmail.com>
2023-05-19 12:02:33 -0300

    style: start styled web page app

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="font-bold">
-      Hello World
-    </h1>
+    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">
+        Sua cápsula do tempo
+      </h1>
+    </div>
+
   )
 }
