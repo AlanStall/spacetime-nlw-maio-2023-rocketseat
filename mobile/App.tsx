615952bbb6c302471e29f231a393f27f90917896
alan-stall <alan_kstall@hotmail.com>
2023-05-20 20:21:07 -0300

    style: finished config fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <View className='bg-zinc-900 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 font-bold text-5xl'>Hello World</Text>
+      <Text className='text-zinc-50 font-title text-5xl'>Hello World</Text>
       <StatusBar style="light" translucent/>
     </View>
   );
