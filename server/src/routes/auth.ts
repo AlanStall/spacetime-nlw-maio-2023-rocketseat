456bfe4b375459719d8e91829e3933e750208422
alan-stall <alan_kstall@hotmail.com>
2023-05-21 11:13:20 -0300

    feat: added accessTokenResponse POST with axios

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import axios from 'axios'
 import { z } from 'zod'
 
 export async function authRoutes(app: FastifyInstance) {
@@ -8,5 +9,26 @@ export async function authRoutes(app: FastifyInstance) {
     })
 
     const { code } = bodySchema.parse(request.body)
+
+    const accessTokenResponse = await axios.post(
+      'http://github.com/login/oauth/access_token',
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    )
+
+    const { access_token } = accessTokenResponse.data
+
+    return {
+      access_token,
+    }
   })
 }
