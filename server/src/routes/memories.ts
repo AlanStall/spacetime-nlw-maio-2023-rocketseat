f59d63903d61e58ab80f7145c41b2bc66733fde3
alan-stall <alan_kstall@hotmail.com>
2023-05-20 22:52:17 -0300

    feat: finished CRUD

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -19,8 +19,6 @@ export async function memoriesRoutes(app: FastifyInstance) {
   })
 
   app.get('/memories/:id', async (request) => {
-    // const { id } = request.params
-
     const paramsSchema = z.object({
       id: z.string().uuid(),
     })
@@ -36,9 +34,65 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.post('/memories/:id', async () => {})
+  app.post('/memories/:id', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
 
-  app.put('/memories/:id', async () => {})
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
 
-  app.delete('/memories/:id', async () => {})
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: 'ibcdiqbsocbojsbcojbqsobc',
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
+
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
 }
