56d2c3121811995055587e172285d57946d85415
alan-stall <alan_kstall@hotmail.com>
2023-05-21 16:39:48 -0300

    chore: install expo-auth-session n config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -15,7 +15,17 @@ import blurBg from './src/assets/bg-blur.png';
 import Stripes from './src/assets/stripes.svg';
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg';
 import { styled } from 'nativewind';
+
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
+
 const StyledStripes = styled(Stripes);
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/56d3ce5bd5175f7b6247',
+};
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -23,6 +33,23 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '56d3ce5bd5175f7b6247',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null
@@ -43,7 +70,11 @@ export default function App() {
           <Text className='text-center font-body text-base leading-relaxed text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
         </View>
 
-        <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-2'>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className='rounded-full bg-green-500 px-5 py-2'
+          onPress={() => signInWithGithub()}
+        >
           <Text className='font-alt text-sm uppercase text-black'>Cadastrar lembrança</Text>
         </TouchableOpacity>
 
