bf753d295758317e2e2d8eed4ef07224e506ca10
alan-stall <alan_kstall@hotmail.com>
2023-05-23 23:48:52 -0300

    feat: finished view memories n styles page web app

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,13 @@
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { api } from '@/lib/api'
 import { cookies } from 'next/headers'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
+
+dayjs.locale(ptBr)
 
 interface Memory {
   id: string
@@ -24,7 +31,7 @@ export default async function Home() {
   })
   const memories: Memory = (await response).data
 
-  if (memories.length === 0) {
+  if (memories.lentgh === 0) {
     return <EmptyMemories />
   }
 
@@ -34,8 +41,25 @@ export default async function Home() {
         return (
           <div key={memory.id} className="space-y-4">
             <time className="befor: befor: -ml-8 flex h-px w-5 items-center gap-2 text-sm text-gray-100 before:bg-gray-50">
-              {memory.createdAt}
+              {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
             </time>
+            <Image
+              src={memory.coverUrl}
+              width={592}
+              height={288}
+              alt=""
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.excerpt}
+            </p>
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-50"
+            >
+              Ler mais...
+              <ArrowRight className="h-4 w-4" />
+            </Link>
           </div>
         )
       })}
